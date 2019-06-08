9e14c2e9dd37f0930af28aca9d5f2e867b72e26c
Alisa <7586237+alisaduncan@users.noreply.github.com>
2019-06-07 21:45:15 -0500

    2 more chapters plus readme on how to access on stackblitz

---
+++ examples/3-component/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>
+      Welcome to {{ title }}!
+    </h1>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo-list';
+}
